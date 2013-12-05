355edc6f34114306fadcaf9108f950e223df1823
hikob <github@hikob.com>
2013-12-05 11:18:35 +0100

    removed PLATFORM_TYPE and PLATFORM_VERSION

--- platform/openlab-fiteco-a8/openlab-main.c
+++ platform/openlab-fiteco-a8/openlab-main.c
@@ -59,11 +59,9 @@ void set_rime_addr()
 	rimeaddr_node_addr.u8[0] = 0xBA;
 	rimeaddr_node_addr.u8[1] = 0xDB;
 	rimeaddr_node_addr.u8[2] = 0x0B;
-        /* Product Type 1 Byte */
-	rimeaddr_node_addr.u8[3] = PLATFORM_TYPE;
-        /* Product Version 1 Byte */
-	rimeaddr_node_addr.u8[4] = PLATFORM_VERSION;
-        /* Serial Number 3 Bytes */
+	/* */
+	rimeaddr_node_addr.u8[3] = uid->uid8[7];
+	rimeaddr_node_addr.u8[4] = uid->uid8[8];
 	rimeaddr_node_addr.u8[5] = uid->uid8[9];
 	rimeaddr_node_addr.u8[6] = uid->uid8[10];
 	rimeaddr_node_addr.u8[7] = uid->uid8[11];
