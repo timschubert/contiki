a36ecbc0575fee1a9d9e03021bb73de1778d52b4
Damien Hedde <damien.hedde@hikob.com>
2014-02-04 18:31:19 +0100

    update openlab-fox rtimer

--- platform/openlab-fox/rtimer-arch.c
+++ platform/openlab-fox/rtimer-arch.c
@@ -25,24 +25,35 @@
  *         Antoine Fraboulet <antoine.fraboulet.at.hikob.com>
  */
 
-#include "platform.h"
-#include "drivers/timer.h"
-#include "drivers/drivers.h"
-#include "platform/agile-fox/agilefox.h"
-#define LOG_LEVEL LOG_LEVEL_DEBUG
-#include "debug.h"
-
 #include "contiki-conf.h"
 #include "sys/energest.h"
 #include "sys/rtimer.h"
 #include "rtimer-arch.h"
- 
+
+#include "stm32/timer_.h"
+#include "stm32f1xx/rcc.h"
+#include "cortex-m3/cm3_nvic_registers.h"
+#include "softtimer/soft_timer_.h"
+
+#define LOG_LEVEL LOG_LEVEL_INFO
+#define NO_DEBUG_HEADER
+#include "lib/debug.h"
+
+/*
+ * rtimer is mapped on the soft_timer
+ */
+#define RTIMER_TIMER        ((const _openlab_timer_t *) softtim.timer)
+#define RTIMER_CHANNEL      softtim.channel
+#define RTIMER_IRQ_LINE     RTIMER_TIMER->irq_line
+
 /*-----------------------------------------------------------------------------------*/
 
-void rtimer_arch_init(void) 
+void rtimer_arch_init(void)
 {
     /* timer is already started in agilefox_drivers.c */
     log_debug("rtimer_arch_init() called");
+    // ensure lowest priority so that rtimer tasks can be interrupted
+    nvic_set_priority(RTIMER_IRQ_LINE, 0xff);
 }
 
 /*-----------------------------------------------------------------------------------*/
@@ -51,12 +62,12 @@ rtimer_clock_t rtimer_arch_now(void)
 {
     return timer_time( RTIMER_TIMER );
 }
- 
+
 /*-----------------------------------------------------------------------------------*/
 
-void rtimer_cb( handler_arg_t arg, uint16_t value ) 
+static void rtimer_cb( handler_arg_t arg, uint16_t value )
 {
-    (void) value; 
+    (void) value;
 
     ENERGEST_ON(ENERGEST_TYPE_IRQ);
     timer_set_channel_compare( RTIMER_TIMER, RTIMER_CHANNEL, 0, NULL, NULL );
