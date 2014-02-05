d81f4536b1ec967b6c43110fa70361484d7b3005
Damien Hedde <damien.hedde@hikob.com>
2014-02-05 11:18:14 +0100

    add dev/uart1.h for openlab platforms

---
+++ platform/openlab/dev/uart1.h
@@ -0,0 +1,30 @@
+/*
+ * This file is part of HiKoB Openlab.
+ *
+ * HiKoB Openlab is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, version 3.
+ *
+ * HiKoB Openlab is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with HiKoB Openlab. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2014 HiKoB.
+ */
+
+/**
+ * \file
+ *         uart1.h
+ *
+ * \author
+ *         Damien Hedde <damien.hedde.at.hikob.com>
+ *
+ */
+
+#define BAUD2UBR(baud) baud
+
