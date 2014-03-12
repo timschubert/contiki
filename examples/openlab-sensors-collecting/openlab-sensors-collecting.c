38aa3162ccd9589bc56d1902d31756eb07a5c3b5
Gaëtan Harter <gaetan.harter@inria.fr>
2014-03-12 16:22:08 +0100

    Add light sensor isl29020 in openlab-fiteco-m3

---
+++ examples/openlab-sensors-collecting/openlab-sensors-collecting.c
@@ -0,0 +1,43 @@
+#include "contiki.h"
+#include <stdio.h>
+#include "dev/light-sensor.h"
+
+PROCESS(sensor_collection, "Sensors collection");
+AUTOSTART_PROCESSES(&sensor_collection);
+
+static void config_light()
+{
+  light_sensor.configure(LIGHT_SENSOR_SOURCE, ISL29020_LIGHT__AMBIENT);
+  light_sensor.configure(LIGHT_SENSOR_RESOLUTION, ISL29020_RESOLUTION__16bit);
+  light_sensor.configure(LIGHT_SENSOR_RANGE, ISL29020_RANGE__1000lux);
+  SENSORS_ACTIVATE(light_sensor);
+}
+static void process_light()
+{
+    int light_val = light_sensor.value(0);
+    float light = ((float)light_val) / LIGHT_SENSOR_VALUE_SCALE;
+
+    printf("light_sensor: %f lux\n", light);
+}
+
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(sensor_collection, ev, data)
+{
+  PROCESS_BEGIN();
+  static struct etimer timer;
+
+  config_light();
+  etimer_set(&timer, CLOCK_SECOND);
+
+  while(1) {
+    PROCESS_WAIT_EVENT();
+    if (ev == PROCESS_EVENT_TIMER) {
+      process_light();
+
+      etimer_set(&timer, CLOCK_SECOND);
+    }
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
---
+++ platform/openlab/dev/light-sensor.h
@@ -0,0 +1,22 @@
+#ifndef LIGHT_SENSOR_H
+#define LIGHT_SENSOR_H
+
+#include "lib/sensors.h"
+#include "isl29020.h"
+
+
+/** Configure types */
+enum {
+  LIGHT_SENSOR_SOURCE = 0,
+  LIGHT_SENSOR_RESOLUTION = 1,
+  LIGHT_SENSOR_RANGE = 2,
+};
+
+enum {
+  // should hold for max RANGE in an int
+  LIGHT_SENSOR_VALUE_SCALE = 32768,
+};
+
+extern const struct sensors_sensor light_sensor;
+
+#endif /* LIGHT-SENSOR_H_ */
