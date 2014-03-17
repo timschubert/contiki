586aeccfd50720ae49dada86e7a69c4a7254b405
Gaëtan Harter <gaetan.harter@inria.fr>
2014-03-17 17:15:40 +0100

    Add magnetometer and temperature support

--- platform/openlab/dev/acc-mag-sensor.h
+++ platform/openlab/dev/acc-mag-sensor.h
@@ -4,15 +4,30 @@
 #include "lib/sensors.h"
 #include "lsm303dlhc.h"
 
-/** Accelerometer available values */
+/** Accelerometer and magnetometer available values */
 enum {
-  ACCELEROMETER_SENSOR_X = 0,
-  ACCELEROMETER_SENSOR_Y = 1,
-  ACCELEROMETER_SENSOR_Z = 2,
+  ACC_MAG_SENSOR_X = 0,
+  ACC_MAG_SENSOR_Y = 1,
+  ACC_MAG_SENSOR_Z = 2,
+  TEMP_SENSOR      = 3,
+};
+
+enum {
+  // check sensor temp values LOOKS STRANGE
+  TEMP_SENSOR_VALUE_SCALE = 64,  // drivers say 256
+};
+
+
+/** Configure types */
+enum {
+  ACC_MAG_SENSOR_DATARATE = 0,
+  ACC_MAG_SENSOR_SCALE = 1,
+  ACC_MAG_SENSOR_MODE = 2,
+
 };
 
 
 extern const struct sensors_sensor acc_sensor;
-extern const struct sensors_sensor mag_sensor;
+extern const struct sensors_sensor mag_temp_sensor;
 
 #endif /* ACC_MAG_SENSOR_H */
