19d7fc1e54e0d658ae46d759fafc88a90e201ccc
Olivier Fambon <olivier.fambon@inria.fr>
2014-05-20 02:40:22 +0200

    simplified examples/ipv6/http-server.c

--- examples/ipv6/http-server/http-server.c
+++ examples/ipv6/http-server/http-server.c
@@ -27,30 +27,21 @@
 #define WEBSERVER_CONF_LOADTIME 0
 #define WEBSERVER_CONF_FILESTATS 0
 #define WEBSERVER_CONF_NEIGHBOR_STATUS 0
-/* Adding links requires a larger RAM buffer. To avoid static allocation
- * the stack can be used for formatting; however tcp retransmissions
- * and multiple connections can result in garbled segments.
- * TODO:use PSOCk_GENERATOR_SEND and tcp state storage to fix this.
- */
-#define WEBSERVER_CONF_ROUTE_LINKS 0
-#if WEBSERVER_CONF_ROUTE_LINKS
-#define BUF_USES_STACK 1
-#endif
 
 static const char *TOP = "<html><head><title>ContikiRPL</title></head><body>\n";
+static const char *SCRIPT = "<script>\
+onload=function(){\
+	a=document.getElementsByTagName('a');\
+	for(i=0;i<a.length;i++)\
+		a[i].href='http://['+a[i].innerHTML+'];'\
+}\
+</script>\n";
 static const char *BOTTOM = "</body></html>\n";
-#if BUF_USES_STACK
-static char *bufptr, *bufend;
-#define ADD(...) do {                                                   \
-    bufptr += snprintf(bufptr, bufend - bufptr, __VA_ARGS__);      \
-  } while(0)
-#else
 static char buf[256];
 static int blen;
 #define ADD(...) do {                                                   \
     blen += snprintf(&buf[blen], sizeof(buf) - blen, __VA_ARGS__);      \
   } while(0)
-#endif
 
 /*---------------------------------------------------------------------------*/
 static void
@@ -79,9 +70,6 @@ PT_THREAD(generate_routes(struct httpd_state *s))
 {
   static uip_ds6_route_t *r;
   static uip_ds6_nbr_t *nbr;
-#if BUF_USES_STACK
-  char buf[256];
-#endif
 #if WEBSERVER_CONF_LOADTIME
   static clock_time_t numticks;
   numticks = clock_time();
@@ -90,11 +78,9 @@ PT_THREAD(generate_routes(struct httpd_state *s))
   PSOCK_BEGIN(&s->sout);
 
   SEND_STRING(&s->sout, TOP);
-#if BUF_USES_STACK
-  bufptr = buf;bufend=bufptr+sizeof(buf);
-#else
   blen = 0;
-#endif
+  SEND_STRING(&s->sout, SCRIPT);
+  blen = 0;
   ADD("Neighbors<pre>");
 
   for(nbr = nbr_table_head(ds6_neighbors);
@@ -102,19 +88,6 @@ PT_THREAD(generate_routes(struct httpd_state *s))
       nbr = nbr_table_next(ds6_neighbors, nbr)) {
 
 #if WEBSERVER_CONF_NEIGHBOR_STATUS
-#if BUF_USES_STACK
-{char* j=bufptr+25;
-      ipaddr_add(&nbr->ipaddr);
-      while (bufptr < j) ADD(" ");
-      switch (nbr->state) {
-      case NBR_INCOMPLETE: ADD(" INCOMPLETE");break;
-      case NBR_REACHABLE: ADD(" REACHABLE");break;
-      case NBR_STALE: ADD(" STALE");break;      
-      case NBR_DELAY: ADD(" DELAY");break;
-      case NBR_PROBE: ADD(" NBR_PROBE");break;
-      }
-}
-#else
 {uint8_t j=blen+25;
       ipaddr_add(&nbr->ipaddr);
       while (blen < j) ADD(" ");
@@ -126,57 +99,25 @@ PT_THREAD(generate_routes(struct httpd_state *s))
       case NBR_PROBE: ADD(" NBR_PROBE");break;
       }
 }
-#endif
 #else
       ipaddr_add(&nbr->ipaddr);
 #endif
 
       ADD("\n");
-#if BUF_USES_STACK
-      if(bufptr > bufend - 45) {
-        SEND_STRING(&s->sout, buf);
-        bufptr = buf; bufend = bufptr + sizeof(buf);
-      }
-#else
       if(blen > sizeof(buf) - 45) {
         SEND_STRING(&s->sout, buf);
         blen = 0;
       }
-#endif
   }
   ADD("</pre>Routes<pre>");
   SEND_STRING(&s->sout, buf);
-#if BUF_USES_STACK
-  bufptr = buf; bufend = bufptr + sizeof(buf);
-#else
   blen = 0;
-#endif
 
   for(r = uip_ds6_route_head(); r != NULL; r = uip_ds6_route_next(r)) {
 
-#if BUF_USES_STACK
-#if WEBSERVER_CONF_ROUTE_LINKS
-    ADD("<a href=http://[");
-    ipaddr_add(&r->ipaddr);
-    ADD("]/status.shtml>");
+    ADD("<a>");
     ipaddr_add(&r->ipaddr);
     ADD("</a>");
-#else
-    ipaddr_add(&r->ipaddr);
-#endif
-#else
-#if WEBSERVER_CONF_ROUTE_LINKS
-    ADD("<a href=http://[");
-    ipaddr_add(&r->ipaddr);
-    ADD("]/status.shtml>");
-    SEND_STRING(&s->sout, buf); //TODO: why tunslip6 needs an output here, wpcapslip does not
-    blen = 0;
-    ipaddr_add(&r->ipaddr);
-    ADD("</a>");
-#else
-    ipaddr_add(&r->ipaddr);
-#endif
-#endif
     ADD("/%u (via ", r->length);
     ipaddr_add(uip_ds6_route_nexthop(r));
     if(1 || (r->state.lifetime < 600)) {
@@ -185,11 +126,7 @@ PT_THREAD(generate_routes(struct httpd_state *s))
       ADD(")\n");
     }
     SEND_STRING(&s->sout, buf);
-#if BUF_USES_STACK
-    bufptr = buf; bufend = bufptr + sizeof(buf);
-#else
     blen = 0;
-#endif
   }
   ADD("</pre>");
 
