0d58719d35e7c5daa4bead9179ee1d8ad21f13c7
Gaëtan Harter <gaetan.harter@inria.fr>
2014-05-26 11:21:30 +0200

    Remove printf call from interrupt context. Remove unneeded calls to uart configuration

--- examples/iotlab/00-hello-world/hello-world.c
+++ examples/iotlab/00-hello-world/hello-world.c
@@ -1,33 +1,20 @@
 #include "contiki.h"
 #include "dev/serial-line.h"
-#include "dev/uart1.h"
 #include <stdio.h>
 
 /*
- * Prints "Hello World !", and echoes whatever arrives on the serial link 
+ * Prints "Hello World !", and echoes whatever arrives on the serial link
  */
 
 PROCESS(serial_echo, "Serial Echo");
 AUTOSTART_PROCESSES(&serial_echo);
 
 /*---------------------------------------------------------------------------*/
-static
-int input_bytes_handler(unsigned char c)
-{
-  printf("%c", c);
-  return serial_line_input_byte(c); // default contiki behaviour
-}
-
-/*---------------------------------------------------------------------------*/
 PROCESS_THREAD(serial_echo, ev, data)
 {
   PROCESS_BEGIN();
-  
-  uart1_set_input(input_bytes_handler);
-  serial_line_init();
 
   printf("Hello World !\n");
-
   while(1) {
     printf("> ");
     PROCESS_YIELD();
@@ -35,7 +22,6 @@ PROCESS_THREAD(serial_echo, ev, data)
       printf("%s\n", (char*)data);
     }
   }
-
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
