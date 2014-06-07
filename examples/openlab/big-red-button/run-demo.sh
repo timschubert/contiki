1a429b9267b811421a42f24f45f5b1637d94afd2
Olivier Fambon <olivier.fambon@inria.fr>
2014-06-07 13:43:30 +0200

    refactored red-button run-demo.sh

--- examples/openlab/big-red-button/run-demo.sh
+++ examples/openlab/big-red-button/run-demo.sh
@@ -3,53 +3,58 @@
 #set -x
 set -e
 
-wpan_prefix=babe::
-wpan_usbtty=/dev/ttyUSB3
-host_prefix=baba::
-host_nwcard=wlan0
-button_ipv6=323:4501:3283:343
+br_device_tty=/dev/ttyUSB1
+button_lladdr=323:4500:3236:3777 # fox36
 
-read -n 1 -p "please disconnect all usb devices, then press any key "
-read -n 1 -p "connect 'red-button' "
-read -n 1 -p "connect 'border-router' "
 
-assert() { $1 &> /dev/null || { echo $2; exit 1; } }
-assert "ifconfig $host_nwcard" "$mypc_nwcard not found"
-assert "ls $wpan_usbtty"       "$wpan_usbtty not found"
-
+assert() { eval "$1" &> /dev/null || { echo $2; exit 1; } }
 STEP() { printf "%-50s" "$1"; }
 
-STEP "we need sudo right"
+STEP "checking tunslip6 & lo"
+tunslip6=$(dirname $0)/../../../tools/tunslip6
+assert "test -x $tunslip" "$tunslip not found"
+assert "ifconfig lo | grep RUNNING" "lo not running"
+echo ok.
+
+STEP "connect the border-router, then press any key"
+read -n 1
+assert "ls $br_device_tty" "border-router: $br_device_tty not found"
+
+STEP "checking sudo right"
 sudo echo ok.
 
 STEP "starting tunslip6 serial/ipv6 bridge"
+baud_rate=$(stty -F $br_device_tty | grep baud | cut -d' ' -f2)
+wpan_prefix=babe::
 (cd ../../../tools
- sudo ./tunslip6 ${wpan_prefix}1/64 -s $wpan_usbtty
-) &> /dev/null &
+ sudo ./tunslip6 $wpan_prefix"1/64" -s $br_device_tty -B $baud_rate
+) &> /tmp/tunslip6.log &
 echo ok.
 
 STEP "waiting for red-button http access"
-while ! curl -sg "http://[$wpan_prefix$button_ipv6]/" > /dev/null ; do
+while ! curl -sg "http://[$wpan_prefix$button_lladdr]/" > /dev/null ; do
 	sleep 1
 done
 echo ok.
 
-STEP "configuring host global ipv6 address"
-#addr=$(ifconfig wlan0 | grep fe80:: | sed 's/.*fe80:://; s:/.*::')
-addr=b00b:beef
-sudo ifconfig wlan0 add $host_prefix$addr/128
+STEP "configuring host lo global ipv6 address"
+host_addr=b00b::2
+sudo ifconfig lo del $host_addr/128 &> /dev/null || true
+sudo ifconfig lo add $host_addr/128
 echo ok.
 
 STEP "configuring red-button destination"
-curl -sg "http://[$wpan_prefix$button_ipv6]/set_destination?$host_prefix$addr"\
+curl -sg "http://[$wpan_prefix$button_lladdr]/set_destination?$host_addr"\
 | grep -q dest_address=
 echo ok.
 
 STEP "starting notification web-server"
 (while true; do
-	nc -6 -l 8000 | grep button_state || break
+	nc -6 -l 8000 | grep button_state || exit 1
 done
 ) &
+ws_pid=$!
+disown %%
 echo ok.
 
 echo
@@ -59,8 +64,9 @@ echo "[press any key to terminate demo]"
 echo
 read -n 1
 
-STEP "deconfiguring global ipv6 & stopping processes"
-sudo ifconfig wlan0 del $host_prefix$addr/128
+STEP "de-configuring host ipv6 & stopping processes"
+sudo ifconfig lo del $host_addr/128
 sudo killall tunslip6
+kill $ws_pid
 killall nc
 echo ok.
