1c7fbb94f664d56b566b09c24461d620d78d2504
Julien Vandaele <julien.vandaele@inria.fr>
2017-03-30 16:08:43 +0200

    Modify light sensor access

--- examples/iotlab/04-er-rest-example/resources/res-light.c
+++ examples/iotlab/04-er-rest-example/resources/res-light.c
@@ -34,6 +34,7 @@
  *      Example resource
  * \author
  *      Matthias Kovatsch <kovatsch@inf.ethz.ch>
+ *      Julien Vandaele <julien.vandaele@inria.fr>
  */
 
 #include "contiki.h"
@@ -48,7 +49,7 @@ static void res_get_handler(void *request, void *response, uint8_t *buffer, uint
 
 /* A simple getter example. Returns the reading from light sensor with a simple etag */
 RESOURCE(res_light,
-         "title=\"Photosynthetic and solar light (supports JSON)\";rt=\"LightSensor\"",
+         "title=\"Ambient light (supports JSON)\";rt=\"LightSensor\"",
          res_get_handler,
          NULL,
          NULL,
@@ -57,25 +58,24 @@ RESOURCE(res_light,
 static void
 res_get_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
-  uint16_t light_photosynthetic = light_sensor.value(LIGHT_SENSOR_PHOTOSYNTHETIC);
-  uint16_t light_solar = light_sensor.value(LIGHT_SENSOR_TOTAL_SOLAR);
+  uint16_t light = light_sensor.value(0) / LIGHT_SENSOR_VALUE_SCALE;
 
   unsigned int accept = -1;
   REST.get_header_accept(request, &accept);
 
   if(accept == -1 || accept == REST.type.TEXT_PLAIN) {
     REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
-    snprintf((char *)buffer, REST_MAX_CHUNK_SIZE, "%u;%u", light_photosynthetic, light_solar);
+    snprintf((char *)buffer, REST_MAX_CHUNK_SIZE, "%u", light);
 
     REST.set_response_payload(response, (uint8_t *)buffer, strlen((char *)buffer));
   } else if(accept == REST.type.APPLICATION_XML) {
     REST.set_header_content_type(response, REST.type.APPLICATION_XML);
-    snprintf((char *)buffer, REST_MAX_CHUNK_SIZE, "<light photosynthetic=\"%u\" solar=\"%u\"/>", light_photosynthetic, light_solar);
+    snprintf((char *)buffer, REST_MAX_CHUNK_SIZE, "<light value=\"%u\"/>", light);
 
     REST.set_response_payload(response, buffer, strlen((char *)buffer));
   } else if(accept == REST.type.APPLICATION_JSON) {
     REST.set_header_content_type(response, REST.type.APPLICATION_JSON);
-    snprintf((char *)buffer, REST_MAX_CHUNK_SIZE, "{'light':{'photosynthetic':%u,'solar':%u}}", light_photosynthetic, light_solar);
+    snprintf((char *)buffer, REST_MAX_CHUNK_SIZE, "{'light':%u}", light);
 
     REST.set_response_payload(response, buffer, strlen((char *)buffer));
   } else {
