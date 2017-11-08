e348d001f2437284694df19ee5c27410201d91fe
Tim Schubert <tim.schubert@tu-bs.de>
2017-11-08 10:40:13 +0100

    Begin porting WSN430 support from https://github.com/iot-lab/wsn430/tree/master/OS/Contiki. This commit will not compile.

---
+++ platform/wsn430/platform-conf.h
@@ -0,0 +1,41 @@
+#ifndef __PLATFORM_CONF_H__
+#define __PLATFORM_CONF_H__
+
+/* define RADIO=WITH_CCXXXX in platform/wsn430/Makefile.wsn430 or application Makefile */
+
+/* the low-level radio driver */
+#ifdef WITH_CC1101
+#define NETSTACK_CONF_RADIO cc1101_radio_driver
+#else
+#define NETSTACK_CONF_RADIO cc2420_radio_driver
+#endif
+
+
+#define CC2420_CONF_SYMBOL_LOOP_COUNT 800
+
+/* Our clock resolution, this is the same as Unix HZ. */
+#define CLOCK_CONF_SECOND 128UL
+
+#define F_CPU 1000000
+#define BAUD2UBR(baud) ((F_CPU/baud))
+
+#define CCIF
+#define CLIF
+
+#define CC_CONF_INLINE inline
+
+#define HAVE_STDINT_H
+#define MSP430_MEMCPY_WORKAROUND 1
+#include "msp430def.h"
+
+
+/* Types for clocks and uip_stats */
+typedef unsigned short uip_stats_t;
+typedef unsigned long clock_time_t;
+typedef unsigned long off_t;
+
+
+#define ROM_ERASE_UNIT_SIZE  512
+#define XMEM_ERASE_UNIT_SIZE (64*1024L)
+
+#endif /* __PLATFORM_CONF_H__ */
